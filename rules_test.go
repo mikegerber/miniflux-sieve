73ad28ca98d87f0b30ff2ffbcce58513fb0c2fd0
Mike Gerber <mike@mike-gerber.de>
2026-09-20 20:05:51 +0200

    ✅ Test tagged, rule with no condition

--- rules_test.go
+++ rules_test.go
@@ -22,3 +22,53 @@ func TestRuleFilterTitleMatches(t *testing.T) {
 		t.Error("ruleFilter() = false, want true")
 	}
 }
+
+func TestRuleFilterTagged(t *testing.T) {
+	rule := rule{
+		When: condition{
+			Tagged: StringList{"ernie", "bert"},
+		},
+	}
+	entry := miniflux.Entry{Title: "News from Sesame Street", Tags: []string{"oscar", "bert"}}
+
+	matched, err := ruleFilter(rule)(entry)
+	if err != nil {
+		t.Fatalf("ruleFilter() returned an unexpected error: %v", err)
+	}
+	if !matched {
+		t.Error("ruleFilter() = false, want true")
+	}
+
+	entry = miniflux.Entry{Title: "News from Sesame Street", Tags: []string{"tiffy", "finchen"}}
+}
+
+func TestRuleFilterTaggedReject(t *testing.T) {
+	rule := rule{
+		When: condition{
+			Tagged: StringList{"ernie", "bert"},
+		},
+	}
+	entry := miniflux.Entry{Title: "News from Sesame Street", Tags: []string{"tiffy", "finchen"}}
+
+	matched, err := ruleFilter(rule)(entry)
+	if err != nil {
+		t.Fatalf("ruleFilter() returned an unexpected error: %v", err)
+	}
+	if matched {
+		t.Error("ruleFilter() = true, want false")
+	}
+
+}
+
+func TestRuleWithNoConditionsIsTrue(t *testing.T) {
+	rule := rule{}
+	entry := miniflux.Entry{Title: "Tiffy meets Oscar"}
+
+	matched, err := ruleFilter(rule)(entry)
+	if err != nil {
+		t.Fatalf("ruleFilter() returned an unexpected error: %v", err)
+	}
+	if !matched {
+		t.Error("ruleFilter() = false, want true")
+	}
+}
